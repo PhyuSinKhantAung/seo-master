7aa27bb116cc225e2d718a03f85a05638a19d690
PhyuSinKhantAung <rollyphyusinkhant7290@gmail.com>
2023-04-11 12:07:17 +0630

    added service-part

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,25 @@
 
 // const inter = Inter({ subsets: ['latin'] })
 
+import About from "@/components/About";
 import Hero from "../components/Hero";
-import { navData, heroData } from "../data.json";
+import { navData, heroData, aboutData } from "../data.json";
 
 export async function getStaticProps() {
   return {
     props: {
       navData,
       heroData,
+      aboutData,
     },
   };
 }
 
-export default function Home() {
+export default function Home({ navData, heroData, aboutData }) {
   return (
-    <div className="bg-blue-50 overflow-hidden max-w-[1600px] mx-auto ">
+    <div className="overflow-hidden max-w-[1600px] mx-auto ">
       <Hero navData={navData} heroData={heroData}></Hero>
+      <About aboutData={aboutData}></About>
     </div>
   );
 }
